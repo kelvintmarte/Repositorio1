8cb634258a4819a3ea8b877f1523a590bddc7ccc
kelvintmarte <kelvintmarte@gmail.com>
2020-11-04 23:36:49 -0400

    first commit

---
+++ Tarea1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+
+{
+    char str [100];
+    
+    printf ("Diga su nombre: ");
+    gets (str);
+    
+    printf(" \nMe llamo:  ");
+    puts (str);
+
+    return 0;
+    
+}
